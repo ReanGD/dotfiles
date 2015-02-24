683c4208a5b6589b583b58100cf2f67b35cfbac6
ReanGD <reangd@yandex.ru>
2015-02-24 22:38:55 +0300

    add me settings group

---
+++ .config/emacs/sys/groups.el
@@ -0,0 +1,5 @@
+(provide 'sys/groups)
+
+(defgroup local-config nil
+  "Config settings."
+  :group 'config)
--- .config/emacs/sys/hooks.el
+++ .config/emacs/sys/hooks.el
@@ -1,27 +1,28 @@
 (provide 'sys/hooks)
 
+
 (defcustom cfg-hook:ui nil
   "ui load"
   :type 'hook
-  :group 'my-config)
+  :group 'local-config)
 
 (defcustom cfg-hook:mode nil
   "common mode load (autocomplete and etc.)"
   :type 'hook
-  :group 'my-config)
+  :group 'local-config)
 
 (defcustom cfg-hook:settings nil
   "settings load (c++ settings and etc.)"
   :type 'hook
-  :group 'my-config)
+  :group 'local-config)
 
 (defcustom cfg-hook:session nil
   "session load"
   :type 'hook
-  :group 'my-config)
+  :group 'local-config)
 
 (defcustom cfg-hook:hotkey nil
   "set hotkeys"
   :type 'hook
-  :group 'my-config)
+  :group 'local-config)
 
