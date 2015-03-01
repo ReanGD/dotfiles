4429f1836bf2593a44f4e32d15654f309f13533a
ReanGD <reangd@yandex.ru>
2015-03-01 03:48:00 +0300

    add company-c-headers

--- .config/emacs/minor/company-cfg.el
+++ .config/emacs/minor/company-cfg.el
@@ -6,6 +6,10 @@
   (global-company-mode t)
   (setq company-minimum-prefix-length 3)
   (company-quickhelp-mode 1)
+  (require 'company-c-headers)
+  (add-to-list 'company-c-headers-path-system "/usr/lib/clang/3.5.1/include")
+  ;(add-to-list 'company-c-headers-path-user "/usr/lib/clang/3.5.1/include")
+  (add-to-list 'company-backends 'company-c-headers)
   (require 'company-statistics)
   (setq company-statistics-size 1000)
   (company-statistics-mode))
@@ -14,5 +18,6 @@
 (cfg:add-package 'company)
 (cfg:add-package 'company-quickhelp)
 (cfg:add-package 'company-statistics)
+(cfg:add-package 'company-c-headers)
 (if cfg-var:use-irony
     (cfg:add-package 'company-irony))
