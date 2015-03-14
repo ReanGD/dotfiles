b4e30223c12e032fb347c1eab0d0a7fc2969e6f7
ReanGD <reangd@yandex.ru>
2015-03-14 22:17:41 +0300

    default untabify mode

--- .config/emacs/edit-config.el
+++ .config/emacs/edit-config.el
@@ -13,20 +13,20 @@
   (require 'undo-tree)
   (global-undo-tree-mode)
   (setq undo-tree-auto-save-history nil
-	undo-tree-mode-lighter " Undo"))
+        undo-tree-mode-lighter " Undo"))
 (add-hook 'cfg-hook:minor-mode 'cfg:undo)
 
 (defun cfg:edit ()
   (delete-selection-mode t)
+  (setq-default indent-tabs-mode nil) ;; отступы делаются пробелами
   (setq shift-select-mode t     ;; move with shift
-        indent-tabs-mode nil    ;; отступы делаются пробелами
         tab-width 2             ;; current tab width
         default-tab-width 2     ;; default tab width
         c-basic-offset 2        ;; tab-width for cpp-lang (js, php, c++, java) can be eq tab-width
         js-indent-level 2       ;; indentation level in JS mode
         css-indent-offset 2     ;; indentation level in CSS mode
         backward-delete-char-untabify-method 'untabify ;; hungry - delete all whitespace, both tabs and spaces
-	)
+        )
   (put 'upcase-region 'disabled nil)
   (put 'downcase-region 'disabled nil)
 
