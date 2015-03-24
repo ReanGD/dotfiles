af41f9a951a15c5d96a08ebbdf65b47d2e099f23
ReanGD <reangd@yandex.ru>
2015-03-25 02:26:22 +0300

    поддержка enum class в emacs + поправил поиск Makefile for compilation

--- .config/emacs/major/cpp-cfg.el
+++ .config/emacs/major/cpp-cfg.el
@@ -1,31 +1,44 @@
 (provide 'major/cpp-cfg)
 
-(defun* get-closest-pathname (&optional (file "Makefile"))
-  "Determine the pathname of the first instance of FILE starting from the current directory towards root.
-This may not do the correct thing in presence of links. If it does not find FILE, then it shall return the name
-of FILE in the current directory, suitable for creation"
-  (let ((root (expand-file-name "/"))) ; the win32 builds should translate this correctly
-    (expand-file-name file
-		      (loop 
-			for d = default-directory then (expand-file-name ".." d)
-			if (file-exists-p (expand-file-name file d))
-			return d
-			if (equal d root)
-			return nil))))
+
+(defun lcl:inside-class-enum-p (pos)
+  "Checks if POS is within the braces of a C++ \"enum class\"."
+  (ignore-errors
+    (save-excursion
+      (goto-char pos)
+      (up-list -1)
+      (backward-sexp 1)
+      (looking-back "enum[ \t]+class[ \t]+[^}]*"))))
+
+(defun lcl:align-enum-class (langelem)
+  (if (lcl:inside-class-enum-p (c-langelem-pos langelem))
+      0
+    (c-lineup-topmost-intro-cont langelem)))
+
+(defun lcl:align-enum-class-closing-brace (langelem)
+  (if (lcl:inside-class-enum-p (c-langelem-pos langelem))
+      '-
+    '+))
+
+(defun lcl:fix-enum-class ()
+  "Setup `c++-mode' to better handle \"class enum\"."
+  (add-to-list 'c-offsets-alist '(topmost-intro-cont . lcl:align-enum-class))
+  (add-to-list 'c-offsets-alist
+               '(statement-cont . lcl:align-enum-class-closing-brace))) 
 
 (defun lcl:cpp-hook ()
   (setq c-default-style "Ellemtel" ;; code-slyle bsd
-	indent-tabs-mode nil       ;; отступы делаются пробелами	
-	tab-width 4                ;; default tab width
-	c-basic-offset 4           ;; tab-width for cpp-lang (js, php, c++, java) can be eq tab-width	
-	)
+        indent-tabs-mode nil       ;; отступы делаются пробелами	
+        tab-width 4                ;; default tab width
+        c-basic-offset 4           ;; tab-width for cpp-lang (js, php, c++, java) can be eq tab-width	
+        )
   (local-unset-key (kbd "M-j"))
   (local-unset-key (kbd "M-e"))
   (local-unset-key (kbd "C-M-j")))
 
 (defun lcl:cpp-compile-hook ()
   (require 'compile)
-  (setq make-dir (file-name-directory (directory-file-name (get-closest-pathname))))
+  (setq make-dir (locate-dominating-file (buffer-file-name) "Makefile"))
   (set (make-local-variable 'compile-command)
        (format "make --no-print-directory -C %s" make-dir)))
 
@@ -33,5 +46,6 @@ of FILE in the current directory, suitable for creation"
   (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
   (add-hook 'c-mode-common-hook 'lcl:cpp-hook)
   (add-hook 'c-mode-common-hook 'lcl:cpp-compile-hook)
+  (add-hook 'c++-mode-hook 'lcl:fix-enum-class)
   )
 (add-hook 'cfg-hook:major-mode 'cfg:cpp)
