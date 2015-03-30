04de9e00dbe93464e6866590d65527ae9ecbbca1
ReanGD <reangd@yandex.ru>
2015-03-31 01:14:05 +0300

    unset local key C-i in yasnippet mode

--- .config/emacs/minor/yasnippet-cfg.el
+++ .config/emacs/minor/yasnippet-cfg.el
@@ -1,8 +1,14 @@
 (provide 'minor/yasnippet-cfg)
 
+(defun lcl:yasnippet-hotheys()
+  (local-unset-key (kbd "C-i"))
+  )
+
 (defun cfg:yasnippet ()
   (require 'yasnippet)
+  (add-hook 'yas-minor-mode-hook 'lcl:yasnippet-hotheys)
   (yas-global-mode 1))
+
 (add-hook 'cfg-hook:minor-mode 'cfg:yasnippet)
 
 (cfg:add-package 'yasnippet)
