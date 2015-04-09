d8225a29dc738d23497df7cd12546dc1c61ecef2
ReanGD <reangd@yandex.ru>
2015-04-10 00:24:48 +0300

    new algoritm set hotkeys

--- .config/emacs/minor/cfg-cfg.el
+++ .config/emacs/minor/cfg-cfg.el
@@ -9,7 +9,6 @@
   cfg-mode-map)
 
 (defadvice load (after give-my-keybindings-priority)
-  "Try to ensure that my keybindings always have priority."
   (if (not (eq (car (car minor-mode-map-alist)) 'cfg-mode))
       (let ((mykeys (assq 'cfg-mode minor-mode-map-alist)))
         (assq-delete-all 'cfg-mode minor-mode-map-alist)
@@ -26,74 +25,76 @@
 
 (define-globalized-minor-mode global-cfg-mode cfg-mode turn-on-cfg-mode)
 
+(defun lcl:get-hotkeys ()
+  (list
+   ;; File
+   (list "C-s" 'save-buffer)              ;; C-x C-s
+   (list "C-x u" 'undo-tree-visualize)
+   (list "M-Z" 'undo-tree-redo)
+   (list "M-z" 'undo-tree-undo)
+   ;; Menu
+   (list "C-o" 'cfg-func:find-file)       ;; C-x C-f
+   (list "C-p" 'cfg-func:find-buffer)     ;; C-x b
+   (list "C-S-p" 'cfg-func:find-command)  ;; M-x
+   (list "M-P" 'cfg-func:find-major-command)
+   (list "C-r" 'idomenu)
+   (list "C-S-r" 'imenu-anywhere)
+   ;; Move
+   (list "M-i" 'previous-line)            ;; C-p
+   (list "M-k" 'next-line)                ;; C-n
+   (list "M-j" 'backward-char)            ;; C-b
+   (list "M-l" 'forward-char)             ;; C-f
+   (list "C-j" 'backward-word)            ;; M-b
+   (list "C-l" 'forward-word)             ;; M-f
+   (list "M-u" 'move-beginning-of-line)   ;; C-a
+   (list "M-o" 'move-end-of-line)         ;; C-e
+   (list "C-S-i" 'md/move-lines-up)
+   (list "C-S-k" 'md/move-lines-down)
+   (list "<prior>" 'cfg:page-up)          ;; prior/M-v
+   (list "<next>" 'cfg:page-down)         ;; next/C-v
+   (list "C-i" 'cfg:scroll-screen-up)
+   (list "<C-up>" 'cfg:scroll-screen-up)
+   (list "C-k" 'cfg:scroll-screen-down)
+   (list "<C-down>" 'cfg:scroll-screen-down)
+   ;; Select
+   (list "C-a"   'mark-whole-buffer)      ;; C-x h
+   ;; Edit
+   (list "M-d" 'cfg:backspace-soft-tab-once)
+   (list "<backspace>" 'cfg:backspace-soft-tab-once)
+   (list "M-f" 'delete-char)              ;; C-d
+   (list "<delete>" 'delete-char)
+   (list "M-e" 'cfg:backward-delete-word)
+   (list "<C-backspace>" 'cfg:backward-delete-word)
+   (list "M-r" 'cfg:delete-word)          ;; M-d
+   (list "<C-delete>" 'cfg:delete-word)
+   (list "M-g" 'cfg:delete-line)          ;; C-k
+   (list "C-S-d" 'md/duplicate-down)
+   (list "C-S-c" 'kill-ring-save)         ;; M-w
+   (list "C-S-v" 'yank)                   ;; C-y
+   (list "C-/"   'comment-dwim-2)
+   (list "<f12>" 'whitespace-mode)
+   ;; Others
+   (list "C-b" 'cfg-func:compile)
+   (if cfg-var:autocomplete
+       (list "C-SPC" cfg-var:autocomplete))
+   ;; My extensions
+   (list "C-x C-f" 'cfg:format-buffer)
+   ;; Search
+   (list "C-f" 'isearch-forward)          ;; C-s
+   ))
+
 (defun cfg:cfg-hotheys (map)
-  ;; File
-  (define-key map (kbd "C-s") 'save-buffer)              ;; C-x C-s
-  (define-key map (kbd "C-x u") 'undo-tree-visualize)
-  (define-key map (kbd "M-Z") 'undo-tree-redo)
-  (define-key map (kbd "M-z") 'undo-tree-undo)
-
-  ;; Menu
-  (define-key map (kbd "C-o") 'cfg-func:find-file)       ;; C-x C-f
-  (define-key map (kbd "C-p") 'cfg-func:find-buffer)     ;; C-x b
-  (define-key map (kbd "C-S-p") 'cfg-func:find-command)  ;; M-x
-  (define-key map (kbd "M-P") 'cfg-func:find-major-command)
-  (define-key map (kbd "C-r") 'idomenu)
-  (define-key map (kbd "C-S-r") 'imenu-anywhere)
-
-  ;; Move
-  (define-key map (kbd "M-i") 'previous-line)            ;; C-p
-  (define-key map (kbd "M-k") 'next-line)                ;; C-n
-  (define-key map (kbd "M-j") 'backward-char)            ;; C-b
-  (define-key map (kbd "M-l") 'forward-char)             ;; C-f
-  (define-key map (kbd "C-j") 'backward-word)            ;; M-b
-  (define-key map (kbd "C-l") 'forward-word)             ;; M-f
-  (define-key map (kbd "M-u") 'move-beginning-of-line)   ;; C-a
-  (define-key map (kbd "M-o") 'move-end-of-line)         ;; C-e
-  (define-key map (kbd "C-S-i") 'md/move-lines-up)
-  (define-key map (kbd "C-S-k") 'md/move-lines-down)
-  (define-key map (kbd "<prior>") 'cfg:page-up)          ;; prior/M-v
-  (define-key map (kbd "<next>") 'cfg:page-down)         ;; next/C-v
-  (define-key map (kbd "C-i") 'cfg:scroll-screen-up)
-  (define-key map (kbd "<C-up>") 'cfg:scroll-screen-up)
-  (define-key map (kbd "C-k") 'cfg:scroll-screen-down)
-  (define-key map (kbd "<C-down>") 'cfg:scroll-screen-down)
-  
-  ;; Select
-  (define-key map (kbd "C-a")   'mark-whole-buffer)      ;; C-x h
-
-  ;; Edit
-  (define-key map (kbd "M-d") 'cfg:backspace-soft-tab-once)
-  (define-key map (kbd "<backspace>") 'cfg:backspace-soft-tab-once)
-  (define-key map (kbd "M-f") 'delete-char)              ;; C-d
-  (define-key map (kbd "<delete>") 'delete-char)
-  (define-key map (kbd "M-e") 'cfg:backward-delete-word)
-  (define-key map (kbd "<C-backspace>") 'cfg:backward-delete-word)
-  (define-key map (kbd "M-r") 'cfg:delete-word)          ;; M-d
-  (define-key map (kbd "<C-delete>") 'cfg:delete-word)
-  (define-key map (kbd "M-g") 'cfg:delete-line)          ;; C-k
-  (define-key map (kbd "C-S-d") 'md/duplicate-down)
-  (define-key map (kbd "C-S-c") 'kill-ring-save)         ;; M-w
-  (define-key map (kbd "C-S-v") 'yank)                   ;; C-y
-  (define-key map (kbd "C-/")   'comment-dwim-2)
-  (define-key map (kbd "<f12>") 'whitespace-mode)
-
-  ;; Others
-  (define-key map (kbd "C-b") 'cfg-func:compile)
-  (if cfg-var:autocomplete
-      (define-key map (kbd "C-SPC") cfg-var:autocomplete))
-
-  ;; My extensions
-  (define-key map (kbd "C-x C-f") 'cfg:format-buffer)
-
-  ;; Search
-  (define-key map (kbd "C-f") 'isearch-forward)          ;; C-s
-  )
+  (dolist (k (lcl:get-hotkeys))
+    (when k
+      (let ((key (kbd (car k)))
+            (func (car (cdr k))))
+        (define-key map key func)
+        (global-set-key key func)
+        ))))
 
 (defun cfg:cfg ()
   (add-hook 'minibuffer-setup-hook 'turn-off-cfg-mode)
   (cfg:cfg-hotheys cfg-mode-map)
-  (global-cfg-mode)
-  )
+  (global-cfg-mode))
 
 (add-hook 'cfg-hook:minor-mode 'cfg:cfg)
