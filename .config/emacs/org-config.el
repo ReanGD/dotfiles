6b7030a1b608f269c0fcc4fd551f35807408f798
ReanGD <reangd@yandex.ru>
2015-04-18 21:58:42 +0300

    syntax coloring in org-mode

--- .config/emacs/org-config.el
+++ .config/emacs/org-config.el
@@ -3,6 +3,8 @@
 
 (defun cfg:org-mode ()
   (require 'org-install)
+  ;; native color scheme for literate programming
+  (setq org-src-fontify-natively 't)
   (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
   (global-font-lock-mode 1)
   (setq org-log-done t)
