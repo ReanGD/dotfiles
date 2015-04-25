8e3cf959ec465d2e8bcb82811b98d068821d0b94
ReanGD <reangd@yandex.ru>
2015-04-25 23:59:17 +0300

    new org mode config

--- .config/emacs/org-config.el
+++ .config/emacs/org-config.el
@@ -2,11 +2,18 @@
 
 
 (defun cfg:org-mode ()
-  (require 'org-install)
+  ;; (require 'org-install)
+  (add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\)$" . org-mode))
+  (require 'org)
   ;; native color scheme for literate programming
   (setq org-src-fontify-natively 't)
-  (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
   (global-font-lock-mode 1)
   (setq org-log-done t)
-  (setq org-agenda-files (list "~/doc/task/new.org")))
+  (setq org-agenda-files (quote ("~/doc/task")))
+  )
+
 (add-hook 'cfg-hook:major-mode 'cfg:org-mode)
+
+;; (global-set-key "\C-cl" 'org-store-link)
+;; (global-set-key "\C-ca" 'org-agenda)
+;; (global-set-key "\C-cb" 'org-iswitchb)
