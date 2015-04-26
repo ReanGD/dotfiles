fe55c9e9416d0581178bf3d9025c765e46a978fb
ReanGD <reangd@yandex.ru>
2015-04-26 18:09:46 +0300

    отшлифовал конфиг для rust

--- .config/emacs/rust-lang.el
+++ .config/emacs/rust-lang.el
@@ -1,28 +1,31 @@
 (provide 'rust-lang)
 
-
-;; http://emacs.stackexchange.com/questions/2137/press-f5-to-save-compile-and-run-current-rust-file
-(defun lcl:rust-save-compile-and-run ()
-  (interactive)
-  (save-buffer)
-  (if (locate-dominating-file (buffer-file-name) "Cargo.toml")
-    (compile "cargo run")
-    (compile
-      (format "rustc %s & %s"
-        (buffer-file-name)
-        (file-name-sans-extension (buffer-file-name))))))
+(defun lcl:rust-compile-hook ()
+  (require 'compile)
+  (set (make-local-variable 'compile-command)
+       (if (locate-dominating-file (buffer-file-name) "Cargo.toml")
+           "cargo run"
+         (format "rustc %s && %s" (buffer-file-name)
+                 (file-name-sans-extension (buffer-file-name))))))
 
 (defun cfg:rust ()
   (autoload 'rust-mode "rust-mode" nil t)
   (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
-  (setq racer-rust-src-path "/home/rean/tmp/rust/src")
-  (setq racer-cmd "/usr/bin/racer")
-  ;; (add-to-list 'load-path "<path-to-racer>/editors")
-  (eval-after-load "rust-mode" '(require 'racer))
-  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+
   (add-hook 'rust-mode-hook
-	    (lambda ()
-	      (define-key rust-mode-map (kbd "<f5>") 'lcl:rust-save-compile-and-run))))
+            (lambda ()
+              (setq indent-tabs-mode nil)
+              (setq tab-width 4)
+              (setq rust-indent-offset 4)))
+
+  (setq racer-rust-src-path "~/.local/share/rust_src/src")
+  (setq racer-cmd "/usr/bin/racer")
+  (eval-after-load "rust-mode" '(require 'racer))
+
+  (eval-after-load 'flycheck
+    '(add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
+  (add-hook 'rust-mode-hook 'lcl:rust-compile-hook))
 (add-hook 'cfg-hook:major-mode 'cfg:rust)
 
 (cfg:add-package 'rust-mode)
