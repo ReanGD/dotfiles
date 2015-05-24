7d0fd27ae6fccd29ce0465766c2e23943bf5e4f5
ReanGD <reangd@yandex.ru>
2015-05-24 22:21:37 +0300

    irony mode config

--- .config/emacs/minor/company-cfg.el
+++ .config/emacs/minor/company-cfg.el
@@ -6,6 +6,8 @@
   (global-company-mode t)
   (setq company-minimum-prefix-length 3)
   (company-quickhelp-mode 1)
+  (if cfg-var:use-irony
+      (add-to-list 'company-backends 'company-irony))
   (require 'company-c-headers)
   (add-to-list 'company-c-headers-path-system "/usr/lib/clang/3.5.1/include")
   ;(add-to-list 'company-c-headers-path-user "/usr/lib/clang/3.5.1/include")
--- .config/emacs/minor/irony-cfg.el
+++ .config/emacs/minor/irony-cfg.el
@@ -1,9 +1,30 @@
 (provide 'minor/irony-cfg)
 
+(defun my-load-irony-options ()
+  (irony-cdb-json-add-compile-commands-path (projectile-project-root)
+                        (concat (projectile-project-root) "compile_commands.json"))
+  (irony-cdb-autosetup-compile-options))
+  ;; (setq make-dir (locate-dominating-file (buffer-file-name) "Makefile"))
+  ;; (set (make-local-variable 'compile-command)
+  ;;      (format "make --no-print-directory -C %s" make-dir)))
+
+(defun my-irony-mode-hook ()
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
 
 (defun cfg:irony ()
   (add-hook 'c++-mode-hook 'irony-mode)
-  (add-hook 'c-mode-hook 'irony-mode))
+  (add-hook 'c-mode-hook 'irony-mode)
+  
+  (add-hook 'c++-mode-hook 'my-load-irony-options)
+  (add-hook 'c-mode-hook 'my-load-irony-options)
+  
+  (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+  (add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
+  )
 (add-hook 'cfg-hook:minor-mode 'cfg:irony)
 
 (cfg:add-package 'irony)
