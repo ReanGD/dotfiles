3a9f3c1a071f32c00d15d3ff2e569a52575cf87e
ReanGD <reangd@yandex.ru>
2015-06-15 01:03:40 +0300

    ignore list for spelling

--- .config/emacs/minor/spelling-cfg.el
+++ .config/emacs/minor/spelling-cfg.el
@@ -1,5 +1,7 @@
 (provide 'minor/spelling-cfg)
 
+(defvar lcl-var:spelling-ignore nil)
+
 (defun lcl:spelling-add-to-dictionary (marked-text)
   (let* ((word (downcase (aref marked-text 0)))
          (dict (if (string-match "[a-zA-Z]" word)
@@ -14,9 +16,28 @@
       (wcheck-mode 0)
       (wcheck-mode 1))))
 
+(defun lcl:spelling-add-to-ignore (marked-text)
+  (let ((word (aref marked-text 0)))
+    (add-to-list 'lcl-var:spelling-ignore word)
+    (message "Added word \"%s\" to the ignore list" word)
+    (wcheck--hook-outline-view-change)))
+
 (defun lcl:spelling-action-menu (marked-text)
-  (cons (cons "[Add to dictionary]" 'lcl:spelling-add-to-dictionary)
-        (wcheck-parser-ispell-suggestions)))
+  (append (wcheck-parser-ispell-suggestions)
+          (list (cons "[Add to dictionary]" 'lcl:spelling-add-to-dictionary)
+                (cons "[Ignore]" 'lcl:spelling-add-to-ignore))))
+
+(defun lcl:delete-list (delete-list list)
+  (dolist (el delete-list)
+    (setq list (remove el list)))
+  list)
+
+(defun lcl:spelling-parser-lines (&rest ignored)
+  (lcl:delete-list lcl-var:spelling-ignore
+                   (delete-dups
+                    (split-string
+                     (buffer-substring-no-properties (point-min) (point-max))
+                     "\n+" t))))
 
 (defun lcl:spelling-hotkeys ()
   (define-key wcheck-mode-map (kbd "M-s RET") 'wcheck-actions)
@@ -35,6 +56,7 @@
    wcheck-language-data
    '(("All"
       (program . "~/.config/emacs/bin/spell_check_text.sh")
+      (parser . lcl:spelling-parser-lines)
       (action-program . "~/.config/emacs/bin/spell_check_word.sh")
       (action-parser . lcl:spelling-action-menu)
       (read-or-skip-faces
