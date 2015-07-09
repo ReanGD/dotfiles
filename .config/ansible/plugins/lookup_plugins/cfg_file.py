aac73a13043eafdaff46fd346cdb7d4be0340792
ReanGD <reangd@yandex.ru>
2015-07-09 19:51:42 +0300

    plugin cfg_file

---
+++ .config/ansible/plugins/lookup_plugins/cfg_file.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+class LookupModule:
+    def __init__(self, **kwargs):
+        pass
+
+    def run(self, terms, variables=None, **kwargs):
+        return ["python2-idna", "python2-notify"]
