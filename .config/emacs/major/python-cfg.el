c6406511c440c1faee169c70fab751c9c6936bbf
ReanGD <reangd@yandex.ru>
2015-07-15 07:52:36 +0300

    поправил python конфигурацию emacs

--- .config/emacs/major/python-cfg.el
+++ .config/emacs/major/python-cfg.el
@@ -2,15 +2,26 @@
 
 
 (defun cfg:python ()
-  (elpy-enable))
+  (setq elpy-modules
+        '(elpy-module-sane-defaults
+          elpy-module-company
+          elpy-module-eldoc
+          elpy-module-flymake
+          ;; elpy-module-highlight-indentation
+          elpy-module-pyvenv
+          elpy-module-yasnippet))
+  (elpy-enable)
+  )
 (add-hook 'cfg-hook:major-mode 'cfg:python)
 
 (defun cfg:python3 ()
+  (interactive)
   (setq python-check-command "flake8"
         python-shell-interpreter "python"
         elpy-rpc-python-command "python"))
 
 (defun cfg:python2 ()
+  (interactive)
   (setq python-check-command "flake8-python2"
         python-shell-interpreter "python2"
         elpy-rpc-python-command "python2"))
