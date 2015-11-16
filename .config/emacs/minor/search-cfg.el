47ff627833f83df2c66baf25bac5290fd6991be1
ReanGD <reangd@yandex.ru>
2015-11-16 20:43:41 +0300

    new search in emacs

--- .config/emacs/minor/search-cfg.el
+++ .config/emacs/minor/search-cfg.el
@@ -1,17 +1,18 @@
 (provide 'minor/search-cfg)
 
 
-(defun lcl:seach-hotkeys ()
-  (define-key isearch-mode-map (kbd "<f3>") 'isearch-repeat-forward)     ;; C-s
-  (define-key isearch-mode-map (kbd "S-<f3>") 'isearch-repeat-backward)  ;; C-r
-  (define-key isearch-mode-map (kbd "<return>") 'isearch-repeat-forward) ;; C-s
-  )
+;; (defun lcl:seach-hotkeys ()
+;;   (define-key isearch-mode-map (kbd "<f3>") 'isearch-repeat-forward)     ;; C-s
+;;   (define-key isearch-mode-map (kbd "S-<f3>") 'isearch-repeat-backward)  ;; C-r
+;;   (define-key isearch-mode-map (kbd "<return>") 'isearch-repeat-forward) ;; C-s
+;;   )
 
 (defun cfg:seach ()
-  (global-anzu-mode +1)
-  (lcl:seach-hotkeys)
+  ;; (lcl:seach-hotkeys)
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-display-style 'fancy)
   )
 (add-hook 'cfg-hook:minor-mode 'cfg:seach)
 
-(cfg:add-package 'anzu)
-;; (cfg:add-package 'isearch+)
+(cfg:add-package 'swiper)
