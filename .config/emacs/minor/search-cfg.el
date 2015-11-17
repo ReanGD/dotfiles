5fee102faed6a865db42ceef21fbe19d052bdbd3
ReanGD <reangd@yandex.ru>
2015-11-17 20:46:01 +0300

    remap hotkeys in search buffer

--- .config/emacs/minor/search-cfg.el
+++ .config/emacs/minor/search-cfg.el
@@ -1,15 +1,14 @@
 (provide 'minor/search-cfg)
 
 
-;; (defun lcl:seach-hotkeys ()
-;;   (define-key isearch-mode-map (kbd "<f3>") 'isearch-repeat-forward)     ;; C-s
-;;   (define-key isearch-mode-map (kbd "S-<f3>") 'isearch-repeat-backward)  ;; C-r
-;;   (define-key isearch-mode-map (kbd "<return>") 'isearch-repeat-forward) ;; C-s
-;;   )
+(defun lcl:seach-hotkeys ()
+  (define-key ivy-minibuffer-map (kbd "M-i") 'ivy-previous-line)
+  (define-key ivy-minibuffer-map (kbd "M-k") 'ivy-next-line)
+  )
 
 (defun cfg:seach ()
-  ;; (lcl:seach-hotkeys)
   (ivy-mode 1)
+  (lcl:seach-hotkeys)
   (setq ivy-use-virtual-buffers t)
   (setq ivy-display-style 'fancy)
   )
