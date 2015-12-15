239f01ff97191fa00cf6948bf57f3a12ddb66c8e
ReanGD <reangd@yandex.ru>
2015-12-15 21:28:13 +0300

    add new go settings

--- .config/emacs/major/go-cfg.el
+++ .config/emacs/major/go-cfg.el
@@ -3,7 +3,13 @@
 
 (defun cfg:go ()
   (add-hook 'before-save-hook 'gofmt-before-save)
+  (add-hook 'go-mode-hook 'go-eldoc-setup)
+  (add-hook 'go-mode-hook (lambda ()
+                            (set (make-local-variable 'company-backends) '(company-go))
+                            (company-mode)))
   )
 (add-hook 'cfg-hook:major-mode 'cfg:go)
 
 (cfg:add-package 'go-mode)
+(cfg:add-package 'go-eldoc)
+(cfg:add-package 'company-go)
