b2263b97562a59a9ceef885bbd980b3a4677220b
ReanGD <reangd@yandex.ru>
2015-12-25 22:50:07 +0300

    new go settings

--- .config/emacs/major/go-cfg.el
+++ .config/emacs/major/go-cfg.el
@@ -4,14 +4,17 @@
 ;; flycheck
 ;; yasnippet
 (defun cfg:go ()
+  (setq gofmt-command "goimports")
   (add-hook 'before-save-hook 'gofmt-before-save)
   (add-hook 'go-mode-hook 'go-eldoc-setup)
   (add-hook 'go-mode-hook (lambda ()
                             (set (make-local-variable 'company-backends) '(company-go))
                             (company-mode)))
+  (load-file "$GOPATH/src/golang.org/x/tools/cmd/oracle/oracle.el")
   )
 (add-hook 'cfg-hook:major-mode 'cfg:go)
 
 (cfg:add-package 'go-mode)
 (cfg:add-package 'go-eldoc)
+(cfg:add-package 'go-rename)
 (cfg:add-package 'company-go)
