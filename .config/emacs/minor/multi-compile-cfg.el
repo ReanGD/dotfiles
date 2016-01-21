b1a5616e113a38151b9ef95cfe16fa54597dcd46
ReanGD <reangd@yandex.ru>
2016-01-21 21:45:50 +0300

    add go compile command

--- .config/emacs/minor/multi-compile-cfg.el
+++ .config/emacs/minor/multi-compile-cfg.el
@@ -1,7 +1,12 @@
 (provide 'minor/multi-compile-cfg)
 
 (defun cfg:multi-compile ()
-  (require 'multi-compile))
-;;(add-hook 'cfg-hook:minor-mode 'cfg:multi-compile)
+  (require 'multi-compile)
+  (add-to-list 'multi-compile-alist
+               '(go-mode . (
+                            ("go-run" . "go run %path")
+                            )))
+  )
+(add-hook 'cfg-hook:minor-mode 'cfg:multi-compile)
 
 (cfg:add-package 'multi-compile)
