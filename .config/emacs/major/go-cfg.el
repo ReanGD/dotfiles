1c62dae43628b78c8d3fb362949e8e659cfcbe97
ReanGD <reangd@yandex.ru>
2016-04-02 03:32:15 +0300

    настройка multi-compile for go

--- .config/emacs/major/go-cfg.el
+++ .config/emacs/major/go-cfg.el
@@ -5,6 +5,7 @@
 (require 'company nil t)
 (require 'flycheck nil t)
 (require 'yasnippet nil t)
+(require 'multi-compile nil t)
 
 (defun cfg:go ()
   "Configure golang."
@@ -16,6 +17,16 @@
   (add-hook 'go-mode-hook (lambda ()
                             (set (make-local-variable 'company-backends) '(company-go))
                             (company-mode)))
+  (add-to-list
+   'multi-compile-alist
+   '(go-mode . (
+                ("go-build-and-run" "n=${PWD##*/} && go build -v -o bin/$n && echo 'build finish' && (cd bin && eval ./$n)"
+                 (multi-compile-locate-file-dir ".git"))
+                ("go-build" "go build -v -o bin/${PWD##*/}"
+                 (multi-compile-locate-file-dir ".git"))
+                ))
+   )
+
   (load-file "$GOPATH_BASE/src/golang.org/x/tools/cmd/oracle/oracle.el")
   )
 (add-hook 'cfg-hook:major-mode 'cfg:go)
--- .config/emacs/minor/multi-compile-cfg.el
+++ .config/emacs/minor/multi-compile-cfg.el
@@ -1,15 +1,6 @@
 (provide 'minor/multi-compile-cfg)
 
 (defun cfg:multi-compile ()
-  (require 'multi-compile)
-  (add-to-list
-   'multi-compile-alist
-   '(go-mode . (("go-build" "go build -v"
-                 (multi-compile-locate-file-dir ".git"))
-                ("go-build-and-run" "go build -v && echo 'build finish' && eval ./${PWD##*/}"
-                 (multi-compile-locate-file-dir ".git"))))
-   )
-
   )
 (add-hook 'cfg-hook:minor-mode 'cfg:multi-compile)
 
