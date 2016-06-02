c641332949c6df51fa17a0226d6d04828bebea49
ReanGD <reangd@yandex.ru>
2016-06-02 20:24:10 +0300

    конфиги Emacs для Go

--- .config/emacs/major/go-cfg.el
+++ .config/emacs/major/go-cfg.el
@@ -6,17 +6,19 @@
 (require 'flycheck nil t)
 (require 'yasnippet nil t)
 (require 'multi-compile nil t)
+(require 'go-eldoc nil t)
+(require 'company-go nil t)
 
 (defun cfg:go ()
   "Configure golang."
   (add-hook 'before-save-hook 'gofmt-before-save)
   (setq-default gofmt-command "goimports")
-  (add-hook 'go-mode-hook 'yas-minor-mode)
-  (add-hook 'go-mode-hook 'flycheck-mode)
   (add-hook 'go-mode-hook 'go-eldoc-setup)
   (add-hook 'go-mode-hook (lambda ()
                             (set (make-local-variable 'company-backends) '(company-go))
                             (company-mode)))
+  (add-hook 'go-mode-hook 'yas-minor-mode)
+  (add-hook 'go-mode-hook 'flycheck-mode)
   (add-to-list
    'multi-compile-alist
    '(go-mode . (
@@ -26,15 +28,17 @@
                  (multi-compile-locate-file-dir ".git"))
                 ))
    )
-
-  (load-file "$GOPATH_BASE/src/golang.org/x/tools/cmd/oracle/oracle.el")
   )
 (add-hook 'cfg-hook:major-mode 'cfg:go)
 
 (cfg:add-package 'go-mode)
 (cfg:add-package 'go-eldoc)
-(cfg:add-package 'go-rename)
 (cfg:add-package 'company-go)
+(cfg:add-package 'go-rename)
+(cfg:add-package 'gotest)
+(cfg:add-package 'go-scratch)
+(cfg:add-package 'go-direx)
+(cfg:add-package 'go-guru)
 
 (provide 'major/go-cfg)
 ;;; go-cfg.el ends here
