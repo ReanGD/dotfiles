bd5f76953e6c39aa7dd61fb100e748a65fed8e23
ReanGD <reangd@yandex.ru>
2016-07-21 20:55:26 +0300

    org-mode export

--- .config/emacs/major/org-cfg.el
+++ .config/emacs/major/org-cfg.el
@@ -1,6 +1,25 @@
 (provide 'major/org-cfg)
+(provide 'ox-html)
+
+(defun cfg:org-mode-blog-html-link (link desc info)
+  (let ((base-link (org-html-link link desc info)))
+    (concat "<a target=\"_blank\"" (substring base-link 2))))
+
+(defun cfg:org-mode-blog-html-final-output (text back-end info)
+  (format "<div dir=\"ltr\" style=\"text-align: left;\" trbidi=\"on\">\n%s\n</div>" text))
+
+(defun cfg:org-mode-blog-html ()
+  (org-export-define-derived-backend 'blog-html 'html
+    :translate-alist '((link . cfg:org-mode-blog-html-link))
+    :filters-alist '((:filter-final-output . cfg:org-mode-blog-html-final-output))
+    ))
+
+(defun cfg:org-mode-export-blog-html ()
+  (interactive)
+  (org-export-to-buffer 'blog-html "blog-html-export-buffer" nil nil nil t))
 
 (defun cfg:org-mode ()
+  (cfg:org-mode-blog-html)
   ;; (require 'org-install)
   (add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\)$" . org-mode))
   (require 'org)
@@ -13,7 +32,7 @@
   (global-set-key "\C-ca" 'org-agenda)
   (setq org-agenda-custom-commands
         '(("е" "List of all TODO entries"
-         ((call-interactively 'org-todo-list)))))
+           ((call-interactively 'org-todo-list)))))
   (setq org-agenda-files (quote ("~/doc/task"))))
 (add-hook 'cfg-hook:major-mode 'cfg:org-mode)
 
