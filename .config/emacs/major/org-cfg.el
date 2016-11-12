a91790d10a30f9b92ae04e29b6e8d52814f368b6
ReanGD <reangd@yandex.ru>
2016-11-12 23:03:07 +0300

    default font for emacs

--- .config/emacs/major/org-cfg.el
+++ .config/emacs/major/org-cfg.el
@@ -1,25 +1,26 @@
 (provide 'major/org-cfg)
-(provide 'ox-html)
+; (provide 'ox-html)
 
-(defun cfg:org-mode-blog-html-link (link desc info)
-  (let ((base-link (org-html-link link desc info)))
-    (concat "<a target=\"_blank\"" (substring base-link 2))))
+; (defun cfg:org-mode-blog-html-link (link desc info)
+;   (let ((base-link (org-html-link link desc info)))
+;     (concat "<a target=\"_blank\"" (substring base-link 2))))
 
-(defun cfg:org-mode-blog-html-final-output (text back-end info)
-  (format "<div dir=\"ltr\" style=\"text-align: left;\" trbidi=\"on\">\n%s\n</div>" text))
+; (defun cfg:org-mode-blog-html-final-output (text back-end info)
+;   (format "<div dir=\"ltr\" style=\"text-align: left;\" trbidi=\"on\">\n%s\n</div>" text))
 
-(defun cfg:org-mode-blog-html ()
-  (org-export-define-derived-backend 'blog-html 'html
-    :translate-alist '((link . cfg:org-mode-blog-html-link))
-    :filters-alist '((:filter-final-output . cfg:org-mode-blog-html-final-output))
-    ))
+; (defun cfg:org-mode-blog-html ()
+;   ; (org-export-define-derived-backend 'blog-html 'html
+;   ;   :translate-alist '((link . cfg:org-mode-blog-html-link))
+;   ;   :filters-alist '((:filter-final-output . cfg:org-mode-blog-html-final-output))
+;   ;   )
+;   )
 
-(defun cfg:org-mode-export-blog-html ()
-  (interactive)
-  (org-export-to-buffer 'blog-html "blog-html-export-buffer" nil nil nil t))
+; (defun cfg:org-mode-export-blog-html ()
+;   (interactive)
+;   (org-export-to-buffer 'blog-html "blog-html-export-buffer" nil nil nil t))
 
 (defun cfg:org-mode ()
-  (cfg:org-mode-blog-html)
+  ; (cfg:org-mode-blog-html)
   ;; (require 'org-install)
   (add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\)$" . org-mode))
   (require 'org)
