f4a373efad4c70e8d2ff5a1f6e9c376c0ace62fe
ReanGD <reangd@yandex.ru>
2017-04-23 04:39:40 +0300

    Вынес обработку сигналов в отдельный файл

---
+++ .config/awesome/config/signals-config.lua
@@ -0,0 +1,38 @@
+local awful = require("awful")
+local beautiful = require("beautiful")
+
+local signals = {}
+
+
+function signals:init()
+    -- actions on every application start
+    client.connect_signal(
+        "manage",
+        function(c)
+            -- startup placement
+            if awesome.startup
+               and not c.size_hints.user_position
+               and not c.size_hints.program_position
+            then
+                awful.placement.no_offscreen(c)
+            end
+        end
+    )
+
+    -- Enable sloppy focus, so that focus follows mouse.
+    client.connect_signal(
+        "mouse::enter",
+        function(c)
+            if awful.layout.get(c.screen) ~= awful.layout.suit.magnifier
+               and awful.client.focus.filter(c)
+            then
+                client.focus = c
+            end
+        end
+    )
+
+    client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
+    client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+end
+
+return signals
--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -89,56 +89,10 @@ hotkeys:init(modkey)
 local rules = require("config.rules-config")
 rules:init(hotkeys)
 
--- {{{ Signals
--- Signal function to execute when a new client appears.
-client.connect_signal("manage", function (c)
-    -- Set the windows at the slave,
-    -- i.e. put it at the end of others instead of setting it master.
-    -- if not awesome.startup then awful.client.setslave(c) end
+local signals = require("config.signals-config")
+signals:init()
 
-    if awesome.startup and
-      not c.size_hints.user_position
-      and not c.size_hints.program_position then
-        -- Prevent clients from being unreachable after screen count changes.
-        awful.placement.no_offscreen(c)
-    end
-end)
-
--- Add a titlebar if titlebars_enabled is set to true in the rules.
-client.connect_signal("request::titlebars", function(c)
-    -- buttons for the titlebar
-    local buttons = gears.table.join(
-        awful.button({ }, 1, function()
-            client.focus = c
-            c:raise()
-            awful.mouse.client.move(c)
-        end),
-        awful.button({ }, 3, function()
-            client.focus = c
-            c:raise()
-            awful.mouse.client.resize(c)
-        end)
-    )
-end)
-
--- Enable sloppy focus, so that focus follows mouse.
-client.connect_signal("mouse::enter", function(c)
-    if awful.layout.get(c.screen) ~= awful.layout.suit.magnifier
-        and awful.client.focus.filter(c) then
-        client.focus = c
-    end
-end)
-
-client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
-client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
--- }}}
-
-
-
--- Autostart user applications
------------------------------------------------------------------------------------------------------------------------
-local autostart = require("config.autostart-config") -- load file with autostart application list
-
+local autostart = require("config.autostart-config")
 if timestamp.is_startup() then
   autostart.run()
 end
