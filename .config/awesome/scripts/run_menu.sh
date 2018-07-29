0d3d84ef26bf358d8c6df94bea07f069f6685b01
ReanGD <reangd@yandex.ru>
2018-07-30 00:44:46 +0300

    command for tv

--- .config/awesome/scripts/run_menu.sh
+++ .config/awesome/scripts/run_menu.sh
@@ -1,5 +1,19 @@
-res=$(echo -e "reboot\npoweroff\nsuspend\nhibernate" | rofi -dmenu)
-if [ $? -eq 0 ];
+operation=$(echo -e "reboot\npoweroff\nsuspend\nhibernate\ntv_on\ntv_off" | rofi -dmenu)
+res=$?
+
+if [ $res -ne 0 ]
 then
-  systemctl $res
+	exit $res  
 fi
+
+case "$operation" in
+	"reboot" | "poweroff" | "suspend" | "hibernate")
+	systemctl $operation
+	;;
+	"tv_on")
+	xrandr --output HDMI-0 --primary --auto --output HDMI-1 --left-of HDMI-0 --auto
+	;;
+	"tv_off")
+	xrandr --output HDMI-1 --off
+	;;
+esac
