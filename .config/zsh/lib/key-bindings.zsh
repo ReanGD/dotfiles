be48fde233de323ece1573f49f6a27c3a3fa5848
ReanGD <reangd@yandex.ru>
2020-07-13 02:09:24 +0300

    refactoring keybindings for move

--- .config/zsh/lib/key-bindings.zsh
+++ .config/zsh/lib/key-bindings.zsh
@@ -1,101 +1,45 @@
-# http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html
-# http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Zle-Builtins
-# http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Standard-Widgets
+# list of keymaps: bindkey -l
+# hotkeys in keymap: bindkey -M rean
+# ^[u = alt+u
+# ^U = ctrl+U
+# for get key code - exec "cat > /dev/null" and press keys:
 
-# Make sure that the terminal is in application mode when zle is active, since
-# only then values from $terminfo are valid
-if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
-  function zle-line-init() {
-    echoti smkx
-  }
-  function zle-line-finish() {
-    echoti rmkx
-  }
-  zle -N zle-line-init
-  zle -N zle-line-finish
-fi
+bindkey -N rean emacs
+bindkey -A rean main
 
-bindkey -e                                            # Use emacs key bindings
+# create a zkbd compatible hash
+typeset -g -A key
+key[Home]="${terminfo[khome]}"
+key[End]="${terminfo[kend]}"
+key[Left]="${terminfo[kcub1]}"
+key[Right]="${terminfo[kcuf1]}"
+key[Control-Left]="^[Od"
+key[Control-Right]="^[Oc"
+
+key[Insert]="${terminfo[kich1]}"
+key[Backspace]="${terminfo[kbs]}"
+key[Delete]="${terminfo[kdch1]}"
+key[Up]="${terminfo[kcuu1]}"
+key[Down]="${terminfo[kcud1]}"
+key[PageUp]="${terminfo[kpp]}"
+key[PageDown]="${terminfo[knp]}"
+key[Shift-Tab]="${terminfo[kcbt]}"
+
+# Move
+bindkey '^[l' forward-char
+bindkey "${key[Right]}" forward-char
+
+bindkey '^[j' backward-char
+bindkey "${key[Left]}" backward-char
+
+bindkey '^[u' beginning-of-line
+bindkey "${key[Home]}" beginning-of-line
+
+bindkey '^[o' end-of-line
+bindkey "${key[End]}" end-of-line
 
 bindkey '^L' forward-word
+bindkey "${key[Control-Right]}" forward-word
+
 bindkey '^J' backward-word
-bindkey '\ee' backward-kill-word
-bindkey '\er' kill-word
-bindkey '\ei' up-line-or-history
-bindkey '\ek' down-line-or-history
-bindkey '\el' forward-char
-bindkey '\ej' backward-char
-bindkey '\eu' beginning-of-line
-bindkey '\eo' end-of-line
-bindkey '\ef' delete-char
-bindkey '\ed' backward-delete-char
-bindkey '\eg' backward-kill-line
-
-# bindkey '\ew' kill-region                             # [Esc-w] - Kill from the cursor to the mark
-# bindkey -s '\el' 'ls\n'                               # [Esc-l] - run command: ls
-# bindkey '^r' history-incremental-search-backward      # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
-if [[ "${terminfo[kpp]}" != "" ]]; then
-  bindkey "${terminfo[kpp]}" up-line-or-history       # [PageUp] - Up a line of history
-fi
-if [[ "${terminfo[knp]}" != "" ]]; then
-  bindkey "${terminfo[knp]}" down-line-or-history     # [PageDown] - Down a line of history
-fi
-
-if [[ "${terminfo[kcuu1]}" != "" ]]; then
-  bindkey "${terminfo[kcuu1]}" up-line-or-search      # start typing + [Up-Arrow] - fuzzy find history forward
-fi
-if [[ "${terminfo[kcud1]}" != "" ]]; then
-  bindkey "${terminfo[kcud1]}" down-line-or-search    # start typing + [Down-Arrow] - fuzzy find history backward
-fi
-
-if [[ "${terminfo[khome]}" != "" ]]; then
-  bindkey "${terminfo[khome]}" beginning-of-line      # [Home] - Go to beginning of line
-fi
-if [[ "${terminfo[kend]}" != "" ]]; then
-  bindkey "${terminfo[kend]}"  end-of-line            # [End] - Go to end of line
-fi
-
-bindkey ' ' magic-space                               # [Space] - do history expansion
-
-bindkey '^[[1;5C' forward-word                        # [Ctrl-RightArrow] - move forward one word
-bindkey '^[[1;5D' backward-word                       # [Ctrl-LeftArrow] - move backward one word
-
-if [[ "${terminfo[kcbt]}" != "" ]]; then
-  bindkey "${terminfo[kcbt]}" reverse-menu-complete   # [Shift-Tab] - move through the completion menu backwards
-fi
-
-bindkey '^?' backward-delete-char                     # [Backspace] - delete backward
-if [[ "${terminfo[kdch1]}" != "" ]]; then
-  bindkey "${terminfo[kdch1]}" delete-char            # [Delete] - delete forward
-else
-  bindkey "^[[3~" delete-char
-  bindkey "^[3;5~" delete-char
-  bindkey "\e[3~" delete-char
-fi
-
-# Edit the current command line in $EDITOR
-autoload -U edit-command-line
-zle -N edit-command-line
-bindkey '\C-x\C-e' edit-command-line
-
-# file rename magick
-bindkey "^[m" copy-prev-shell-word
-
-# consider emacs keybindings:
-
-#bindkey -e  ## emacs key bindings
-#
-#bindkey '^[[A' up-line-or-search
-#bindkey '^[[B' down-line-or-search
-#bindkey '^[^[[C' emacs-forward-word
-#bindkey '^[^[[D' emacs-backward-word
-#
-#bindkey -s '^X^Z' '%-^M'
-#bindkey '^[e' expand-cmd-path
-#bindkey '^[^I' reverse-menu-complete
-#bindkey '^X^N' accept-and-infer-next-history
-#bindkey '^W' kill-region
-#bindkey '^I' complete-word
-## Fix weird sequence that rxvt produces
-#bindkey -s '^[[Z' '\t'
-#
+bindkey "${key[Control-Left]}" backward-word
