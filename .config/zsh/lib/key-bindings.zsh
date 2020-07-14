4124b32059add1af45e5dc88fb0b4868ba1c6783
ReanGD <reangd@yandex.ru>
2020-07-15 01:30:58 +0300

    add paste mode

--- .config/zsh/lib/key-bindings.zsh
+++ .config/zsh/lib/key-bindings.zsh
@@ -7,6 +7,7 @@
 
 source $ZSH_CUSTOM/lib/library.zsh
 
+bindkey -N paste
 bindkey -N rean emacs
 bindkey -A rean main
 
@@ -20,6 +21,7 @@ key[End]="${terminfo[kend]}"
 key[PageUp]="${terminfo[kpp]}"
 key[PageDown]="${terminfo[knp]}"
 
+key[Delete]="${terminfo[kdch1]}"
 key[Ctrl-Delete]="^[[3^"
 key[Backspace]="${terminfo[kbs]}"
 key[Ctrl-Backspace]="^[[33~"
@@ -30,6 +32,10 @@ key[Right]="${terminfo[kcuf1]}"
 key[Ctrl-Right]="^[Oc"
 key[Up]="${terminfo[kcuu1]}"
 key[Down]="${terminfo[kcud1]}"
+
+key[Start-Paste]="^[[200~"
+key[End-Paste]="^[[201~"
+
 
 # Move
 bindkey '^[l' forward-char
@@ -50,6 +56,7 @@ bindkey "${key[Ctrl-Right]}" forward-word
 bindkey '^J' backward-word
 bindkey "${key[Ctrl-Left]}" backward-word
 
+
 # Edit
 bindkey '^[f' delete-char
 bindkey "${key[Delete]}" delete-char
@@ -67,8 +74,9 @@ bindkey '^[g' kill-whole-line
 
 bindkey '^[t' clear-screen
 
-zle -N edit-command-line-sublime
-bindkey '^X^E' edit-command-line-sublime
+zle -N lib-edit-command-line-sublime
+bindkey '^X^E' lib-edit-command-line-sublime
+
 
 # History
 bindkey '^[i' up-line-or-search
@@ -82,3 +90,27 @@ bindkey "${key[PageDown]}" down-line-or-history
 
 bindkey "${key[Tab]}" menu-expand-or-complete
 bindkey "${key[Shift-Tab]}" reverse-menu-complete
+
+
+# Paste mode
+# see: https://github.com/zsh-users/zsh-autosuggestions/issues/141#issuecomment-280876210
+zle -N lib-start-paste
+bindkey "${key[Start-Paste]}" lib-start-paste
+
+zle -N lib-end-paste
+bindkey -M paste "${key[End-Paste]}" lib-end-paste
+
+zle -N paste-insert lib-paste-insert
+bindkey -R -M paste "^@"-"\M-^?" paste-insert
+bindkey -M paste -s '^M' '^J'
+
+
+# Finally, make sure the terminal is in application mode, when zle is
+# active. Only then are the values from $terminfo valid.
+if (( ${+terminfo[smkx]} && ${+terminfo[rmkx]} )); then
+	autoload -Uz add-zle-hook-widget
+	function zle_application_mode_start { echoti smkx }
+	function zle_application_mode_stop { echoti rmkx }
+	add-zle-hook-widget -Uz zle-line-init zle_application_mode_start
+	add-zle-hook-widget -Uz zle-line-finish zle_application_mode_stop
+fi
--- .config/zsh/lib/library.zsh
+++ .config/zsh/lib/library.zsh
@@ -1,5 +1,10 @@
 # Edit the current command line in sublime
-edit-command-line-sublime () {
+# for see origin:
+# autoload -z edit-command-line
+# zle -N edit-command-line
+# edit-command-line
+# whence -f edit-command-line
+lib-edit-command-line-sublime() {
 	emulate -L zsh
 	() {
 		exec < /dev/tty
@@ -10,4 +15,21 @@ edit-command-line-sublime () {
 		print -Rz - "$(<$1)"
 	} =(<<<"$PREBUFFER$BUFFER")
 	zle send-break
+}
+
+# switch to paste keymap
+lib-start-paste() {
+    bindkey -A paste main
+}
+
+# restore rean keymap
+lib-end-paste() {
+    bindkey -A rean main
+    LBUFFER+=$_paste_content
+    unset _paste_content
+}
+
+# full paste content
+lib-paste-insert() {
+    _paste_content+=$KEYS
 }
