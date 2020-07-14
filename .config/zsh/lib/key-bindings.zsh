9aa86b101f5b31604ac06bbf5134ba1e3052fb87
ReanGD <reangd@yandex.ru>
2020-07-14 04:59:46 +0300

    add history hotkeys for zsh

--- .config/zsh/lib/key-bindings.zsh
+++ .config/zsh/lib/key-bindings.zsh
@@ -12,23 +12,24 @@ bindkey -A rean main
 
 # create a zkbd compatible hash
 typeset -g -A key
+key[Tab]="\t"
+key[Shift-Tab]="${terminfo[kcbt]}"
+
 key[Home]="${terminfo[khome]}"
 key[End]="${terminfo[kend]}"
-key[Delete]="${terminfo[kdch1]}"
+key[PageUp]="${terminfo[kpp]}"
+key[PageDown]="${terminfo[knp]}"
+
 key[Ctrl-Delete]="^[[3^"
 key[Backspace]="${terminfo[kbs]}"
 key[Ctrl-Backspace]="^[[33~"
+
 key[Left]="${terminfo[kcub1]}"
 key[Ctrl-Left]="^[Od"
 key[Right]="${terminfo[kcuf1]}"
 key[Ctrl-Right]="^[Oc"
-
-key[Insert]="${terminfo[kich1]}"
 key[Up]="${terminfo[kcuu1]}"
 key[Down]="${terminfo[kcud1]}"
-key[PageUp]="${terminfo[kpp]}"
-key[PageDown]="${terminfo[knp]}"
-key[Shift-Tab]="${terminfo[kcbt]}"
 
 # Move
 bindkey '^[l' forward-char
@@ -68,3 +69,16 @@ bindkey '^[t' clear-screen
 
 zle -N edit-command-line-sublime
 bindkey '^X^E' edit-command-line-sublime
+
+# History
+bindkey '^[i' up-line-or-search
+bindkey "${key[Up]}" up-line-or-search
+
+bindkey '^[k' down-line-or-search
+bindkey "${key[Down]}" down-line-or-search
+
+bindkey "${key[PageUp]}" up-line-or-history
+bindkey "${key[PageDown]}" down-line-or-history
+
+bindkey "${key[Tab]}" menu-expand-or-complete
+bindkey "${key[Shift-Tab]}" reverse-menu-complete
