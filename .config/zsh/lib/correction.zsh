1f5c80a6be58857adfb60c8087aebc02c08aec41
ReanGD <reangd@yandex.ru>
2020-07-18 03:02:49 +0300

    add correction settings

---
+++ .config/zsh/lib/correction.zsh
@@ -0,0 +1,11 @@
+# Disable correction for command
+# alias cp='nocorrect cp'
+# alias man='nocorrect man'
+# alias mkdir='nocorrect mkdir'
+# alias mv='nocorrect mv'
+# alias sudo='nocorrect sudo'
+# or use env var: CORRECT_IGNORE (see doc)
+# for disable correc files use: CORRECT_IGNORE_FILE (see doc)
+
+setopt CORRECT      # Try to correct the spelling of commands
+setopt CORRECT_ALL  # Try to correct the spelling of all arguments in a line
