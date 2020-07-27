68a00238b2e6c46e025c0160f011e500850990f8
ReanGD <reangd@yandex.ru>
2020-07-27 03:39:22 +0300

    add systray widget

---
+++ .config/awesome/widget/systray/init.lua
@@ -0,0 +1,18 @@
+local wibox = require("wibox")
+
+-- Initialize tables and vars for module
+--------------------------------------------------------------------------------
+local systray = {}
+
+-- Module functions
+--------------------------------------------------------------------------------
+
+-- Constructor
+--------------------------------------------------------------------------------
+function systray:init(args)
+	args = args or {}
+
+	self.widget = wibox.widget.systray()
+end
+
+return systray
