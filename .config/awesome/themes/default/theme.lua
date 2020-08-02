1be3a548565d550ede2f25e87adf68511d36d2d4
ReanGD <reangd@yandex.ru>
2020-08-03 00:41:10 +0300

    minimal scheme

--- .config/awesome/themes/default/theme.lua
+++ .config/awesome/themes/default/theme.lua
@@ -1,41 +1,72 @@
-local gfs = require("gears.filesystem")
+local gears = require('gears')
+local beautiful = require('beautiful')
 
--- load base theme
-local default_themes_path = gfs.get_themes_dir().."default/"
-local path = package.path
-package.path = package.path .. ";" .. gfs.get_themes_dir() .. "?.lua"
-local theme = require("default.theme")
-package.path = path
+-- Initialize tables and vars for module
+--------------------------------------------------------------------------------
+local theme = {}
 
--- override vars
-local xresources = require("beautiful.xresources")
-theme.path = gfs.get_configuration_dir() .. "themes/default"
+local gfs = gears.filesystem
+local dpi = beautiful.xresources.apply_dpi
 
-theme.font          = "Ubuntu 11"
-theme.taglist_font  = theme.font
-theme.mono_font     = "Ubuntu Mono 11"
+-- Module functions
+--------------------------------------------------------------------------------
+function theme:solarized_colors()
+    self.bg_normal = "#002b36ff"
+    self.bg_focus  = "#586e75ff"
+    self.fg_normal = "#93a1a1ff"
+    self.fg_focus  = "#fdf6e3ff"
+end
 
-theme.bg_normal     = "#222222"
-theme.bg_focus      = theme.bg_normal
-theme.bg_urgent     = "#ff0000"
-theme.bg_minimize   = "#444444"
-theme.bg_systray    = theme.bg_normal
-theme.fg_normal     = "#aaaaaa"
-theme.fg_focus      = "#ffffff"
-theme.fg_urgent     = "#ffffff"
-theme.fg_minimize   = "#ffffff"
+function theme:client()
+    self.useless_gap   = 0
+    self.border_width  = dpi(1)
+    self.border_normal = "#000000"
+    self.border_focus  = self.bg_focus
+end
 
-theme.useless_gap   = 0
-theme.border_width  = xresources.apply_dpi(1)
-theme.border_normal = "#000000"
-theme.border_focus  = "#535d6c"
-theme.border_marked = "#91231c"
+function theme:tasklist()
+    self.tasklist_bg_focus = self.bg_normal
+    self.tasklist_fg_focus = self.fg_normal
+end
 
-theme.tasklist_disable_icon = true
-theme.taglist_squares_sel = nil
-theme.taglist_squares_unsel = nil
+function theme:layout()
+    local default_themes_path = gfs.get_themes_dir().."default/"
+    local l = default_themes_path .. "layouts/"
 
-theme.icon_theme = nil
-theme.wallpaper = theme.path .. "/wallpapers/morning-wallpaper.jpg"
+    self.layout_fairh      = l .. "fairhw.png"
+    self.layout_fairv      = l .. "fairvw.png"
+    self.layout_floating   = l .. "floatingw.png"
+    self.layout_magnifier  = l .. "magnifierw.png"
+    self.layout_max        = l .. "maxw.png"
+    self.layout_fullscreen = l .. "fullscreenw.png"
+    self.layout_tilebottom = l .. "tilebottomw.png"
+    self.layout_tileleft   = l .. "tileleftw.png"
+    self.layout_tile       = l .. "tilew.png"
+    self.layout_tiletop    = l .. "tiletopw.png"
+    self.layout_spiral     = l .. "spiralw.png"
+    self.layout_dwindle    = l .. "dwindlew.png"
+    self.layout_cornernw   = l .. "cornernww.png"
+    self.layout_cornerne   = l .. "cornernew.png"
+    self.layout_cornersw   = l .. "cornersww.png"
+    self.layout_cornerse   = l .. "cornersew.png"
+end
+
+-- Constructor
+--------------------------------------------------------------------------------
+function theme:init()
+    -- Common
+    self.font = "Inter Medium 10"
+    self.path = gfs.get_configuration_dir() .. "themes/default"
+
+    self.icon_theme = "/usr/share/icons/mate"
+    self.wallpaper = self.path .. "/wallpapers/morning-wallpaper.jpg"
+
+    self:solarized_colors()
+    self:client()
+    self:tasklist()
+    self:layout()
+end
+
+theme:init()
 
 return theme
