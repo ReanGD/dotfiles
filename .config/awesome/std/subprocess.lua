296c94247abaa878231bc7a7c844397d7bf36780
ReanGD <reangd@yandex.ru>
2020-08-04 22:27:20 +0300

    remove unused lib and add run_once function

--- .config/awesome/std/subprocess.lua
+++ .config/awesome/std/subprocess.lua
@@ -1,9 +1,13 @@
+local awful = require("awful")
+local naughty = require("naughty")
+
 -- Initialize tables and vars for module
 --------------------------------------------------------------------------------
 local subprocess = {}
 
 local io = io
 local assert = assert
+local string = string
 
 -- Run command and read output
 --------------------------------------------------------------------------------
@@ -13,6 +17,31 @@ function subprocess.output_read(cmd)
 	file:close()
 
 	return output
+end
+
+-- Run command if it not running
+--------------------------------------------------------------------------------
+function subprocess.run_once(cmd, args)
+	args = args or {}
+
+	if not cmd or cmd == "" then
+		naughty.notify({
+			title = "Error while run autostart app",
+			text = "arg 'cmd' for run_once empty or nil",
+			preset = naughty.config.presets.critical
+		})
+		return
+	end
+
+	local process_name = args.process_name or cmd
+	local run_cmd = string.format("pgrep -u $USER %s > /dev/null || %s", process_name, cmd)
+
+	local params = {}
+	if args.tag then
+		params.tag = args.tag
+	end
+
+	awful.spawn.spawn({ awful.util.shell, "-c", run_cmd }, params)
 end
 
 return subprocess
