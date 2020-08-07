f9fb5641a0fca5caf260433d3f1792ac930500bd
ReanGD <reangd@yandex.ru>
2020-08-07 22:42:01 +0300

    fix caps lock only for awesome

--- .config/start/startx_profile.sh
+++ .config/start/startx_profile.sh
@@ -1,7 +1,15 @@
 #!bin/sh
 
+DE=$1
+
 xrdb -merge ~/.config/start/xresources
-# disable SHIFT+CAPS LOCK
+
+if [ "$DE" = "awesome" ]; then
+# see
+# xmodmap -pke | grep 66
+# less /usr/include/X11/XF86keysym.h
+
+# caps lock as layout switch
 xmodmap -e 'keycode 66 = ISO_Next_Group ISO_Next_Group ISO_Next_Group ISO_Next_Group'
 
-echo $(date) >> ~/xdate
+fi
