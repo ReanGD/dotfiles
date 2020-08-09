fe693156b2d32657281e85ce4f2b479a6343d575
ReanGD <reangd@yandex.ru>
2020-08-10 02:28:48 +0300

    add caps lock settings

--- .config/start/awesome_profile.sh
+++ .config/start/awesome_profile.sh
@@ -3,9 +3,14 @@
 
 xrdb -merge ~/.config/start/xresources
 
+
+# caps lock as layout switch v1
+# see: https://wiki.archlinux.org/index.php/Xorg/Keyboard_configuration#Using_setxkbmap
+setxkbmap -option grp:caps_toggle
+
+# caps lock as layout switch v2
 # see
 # xmodmap -pke | grep 66
 # less /usr/include/X11/XF86keysym.h
-
-# caps lock as layout switch
-xmodmap -e 'keycode 66 = ISO_Next_Group ISO_Next_Group ISO_Next_Group ISO_Next_Group'
+#
+# xmodmap -e 'keycode 66 = ISO_Next_Group ISO_Next_Group ISO_Next_Group ISO_Next_Group'
