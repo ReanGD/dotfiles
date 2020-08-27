5c11a5708a2c0aaab7be698e6dcdfc1f3c66696a
ReanGD <reangd@yandex.ru>
2020-08-28 00:05:39 +0300

    add custom chrome desctop file and use custom firefox profile

--- .local/share/firefox/.mozilla/firefox/rean.default-release/user.js
+++ .local/share/firefox/.mozilla/firefox/rean.default-release/user.js
@@ -4,6 +4,11 @@ user_pref("browser.download.panel.shown", true);
 user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"home-button\",\"urlbar-container\",\"downloads-button\",\"support_lastpass_com-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"bookmarks-menu-button\",\"fxa-toolbar-menu-button\",\"jid1-93wyvpgvxzgatw_jetpack-browser-action\",\"_b2e3360c-a72c-4ba4-813c-603a1fa34356_-browser-action\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"developer-button\",\"support_lastpass_com-browser-action\",\"jid1-93wyvpgvxzgatw_jetpack-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"_2e5ff8c8-32fe-46d0-9fc8-6b8986621f3c_-browser-action\",\"_b2e3360c-a72c-4ba4-813c-603a1fa34356_-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"toolbar-menubar\",\"TabsToolbar\",\"PersonalToolbar\"],\"currentVersion\":16,\"newElementCount\":5}");
 user_pref("browser.uidensity", 1);
 user_pref("browser.urlbar.placeholderName", "Google");
-user_pref("browser.search.hiddenOneOffs", "Википедия (ru)");
+user_pref("browser.search.hiddenOneOffs", "Википедия (ru),Поиск Mail.Ru,OZON.ru,Price.ru");
+user_pref("browser.urlbar.placeholderName", "Google");
 user_pref("network.proxy.type", 0);
 user_pref("network.proxy.autoconfig_url", "https://antizapret.prostovpn.org/proxy.pac");
+user_pref("privacy.trackingprotection.enabled", true);
+// disable telemetry
+user_pref("toolkit.telemetry.enabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
