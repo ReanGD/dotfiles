2f161e464b8cfb9ee47a1e795e88755077dea0f6
ReanGD <reangd@yandex.ru>
2020-09-05 01:10:38 +0300

    sender implementation

---
+++ .config/rofi/scripts/proxy/sender.py
@@ -0,0 +1,32 @@
+import sys
+import json
+from typing import List
+from receiver import Receiver
+
+
+class Sender:
+    def __init__(self):
+        self.__help = None
+        self.__input = None
+
+    def set_help(self, text: str):
+        self.__help = text
+
+    def set_input(self, text: str):
+        self.__input = text
+
+    def send(self, receivers: List[Receiver]):
+        lines = []
+        for receiver in receivers:
+            lines.extend(receiver.get_lines())
+
+        req = {"lines": lines}
+        if self.__help is not None:
+            req["help"] = self.__help
+            self.__help = None
+        if self.__input is not None:
+            req["input"] = self.__input
+            self.__input = None
+
+        sys.stdout.write(json.dumps(req) + "\n")
+        sys.stdout.flush()
