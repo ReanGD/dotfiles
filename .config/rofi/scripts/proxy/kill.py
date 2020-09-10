d40167b9e7584670c310a111218526469988a519
ReanGD <reangd@yandex.ru>
2020-09-11 02:29:14 +0300

    implement kill mode

--- .config/rofi/scripts/proxy/kill.py
+++ .config/rofi/scripts/proxy/kill.py
@@ -1,3 +1,6 @@
+import os
+import psutil
+import getpass
 from writer import Writer
 from receiver import Receiver
 from utils import ExitException
@@ -6,12 +9,34 @@ from utils import ExitException
 class Kill(Receiver):
     def __init__(self, writer: Writer):
         super().__init__(writer, group="Kill", activate_word="kill", prompt="kill")
+        self._current_user = getpass.getuser()
 
     def on_init(self):
-        self.add_line("process 1")
+        attrs = ["pid", "cmdline", "username"]
+        for pid, info in [(it.pid, it.info)
+                          for it in psutil.process_iter(attrs)
+                          if it.info['username'] == self._current_user]:
+            cmd = " ".join(info['cmdline'])
+            self.add_line(f"{pid} {cmd}", str(pid))
 
     def on_input(self, text: str):
         pass
 
     def on_enter(self, id_text: str, text: str):
+        pid = int(id_text)
+        if pid == os.getpid():
+            return
+
+        self._kill_with_subprocesses(pid)
         raise ExitException()
+
+    def _kill_with_subprocesses(self, pid):
+        parent = psutil.Process(pid)
+        processes = parent.children(recursive=True)
+        processes.append(parent)
+        for p in processes:
+            p.terminate()
+
+        gone, alive = psutil.wait_procs(processes, timeout=5)
+        for p in alive:
+            p.kill()
