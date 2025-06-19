9742baec53d087a458b43133a0d24baf94df4d44
ReanGD <reangd@yandex.ru>
2025-06-19 11:57:29 +0300

    add sctl and jctl to zsh

--- .config/zsh/app/_system.zsh
+++ .config/zsh/app/_system.zsh
@@ -42,3 +42,43 @@ if (( $+commands[yay] )); then
 
     _post_compinit+=("compdef _yay yay")
 fi
+
+_sctl_get_command() {
+    for arg in "$@"; do
+        case "$arg" in
+            -*) continue ;;
+            *) echo "$arg"; return ;;
+        esac
+    done
+}
+
+_sctl_needs_sudo() {
+    case "$1" in
+        start|stop|restart|reload|enable|disable|mask|unmask|\
+        reset-failed|daemon-reload|isolate|kill|set-property|\
+        bind|edit|link|revert|set-environment|unset-environment|\
+        import-environment|reload-or-restart|try-restart|reexec|\
+        suspend|hibernate|poweroff|reboot|halt|switch-root|\
+        emergency|rescue)
+            return 0 ;;
+        *)
+            return 1 ;;
+    esac
+}
+
+sctl() {
+    local cmd
+    cmd=$(_sctl_get_command "$@")
+
+    if _sctl_needs_sudo "$cmd"; then
+        sudo systemctl "$@"
+    else
+        systemctl "$@"
+    fi
+}
+
+_post_compinit+=("compdef _systemctl sctl")
+
+alias jctl='sudo journalctl'
+
+_post_compinit+=("compdef _journalctl jctl")
